ce94fe7775dcc158db4c563b99390387c57069d5
andrea <andrea.terenziani.at@gmail.com>
2021-07-02 11:07:46 +0200

    pirullo

--- website/static/js/new_board.js
+++ website/static/js/new_board.js
@@ -1,3 +1,19 @@
 function ok_newBoard() {
     console.log("AAAAA")
+
+    // 1) prendere dati dal modale (how?)
+
+    // 2) inviare richiesta POST al server con fetch
+    /*
+    fetch([url], {
+        method: "POST",
+        credentials: 'same-origin',
+        headers: {
+            "X-CSRFToken": Cookies.get('csrftoken')
+        },
+        body: [dati del modale]
+    }).then((r) => console.log([messaggio di conferma]))
+     */
+
+    // 3) aggiungere linea alla tabella
 }
