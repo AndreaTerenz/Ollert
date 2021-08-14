2f22e491f633703ad61e4a97aa7b56f57509d606
AndreaTerenz <andrea.terenziani.at@gmail.com>
2021-08-14 16:27:33 +0200

    ma perchè rompo sempre qualcosa col refactoring?

--- website/static/js/modals/delete_board.js
+++ website/static/js/modals/delete_board.js
@@ -1,6 +1,6 @@
 var board = ""
 
-addOnShowListener("#deleteModal", event => {
+addOnShowListener("deleteModal", event => {
     // con questo barbatrucco si ottiene il pulsante che ha triggerato l'evento
     let button = event.relatedTarget
     // il pulsante contiene un attributo custom ('data-bs-board') che ha come valore il nome della board a cui è legato
@@ -8,6 +8,7 @@ addOnShowListener("#deleteModal", event => {
     board = button.getAttribute("data-bs-board")
 
     // La domanda da "personalizzare" è in una label all'interno del modale, che viene selezionata
+    let modal = document.getElementById("deleteModal")
     let label = modal.getElementsByTagName("label").item(0)
     // si imposta il testo della label
     label.innerText = "Sei sicuro di voler eliminare la board '" + board + "'?"
