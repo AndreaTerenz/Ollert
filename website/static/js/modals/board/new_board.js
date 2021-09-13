f1d94ff407d87e28a724c3a6a5714b2e0ccc2562
Serena <serena.passini.19@gmail.com>
2021-09-13 18:50:03 +0200

    Aggiunti controlli

--- website/static/js/modals/board/new_board.js
+++ website/static/js/modals/board/new_board.js
@@ -1,13 +1,17 @@
 function ok_newBoard() {
     let modal = document.getElementById("newBoardModal")
     let data = {
-        name : modal.querySelector("#boardName").value,
-        category : modal.querySelector("#selectCategory").value,
+        name: modal.querySelector("#boardName").value,
+        category: modal.querySelector("#selectCategory").value,
         description: modal.querySelector("#boardDescription").value,
-        favorite : modal.querySelector("#favoriteCheck").checked
+        favorite: modal.querySelector("#favoriteCheck").checked
     }
 
-    make_modal_request(data, new_board_url, "newBoardModal", (data) => {
-        insert_html(boards_list_id, data)
-    })
+    if (name != '') {
+        make_modal_request(data, new_board_url, "newBoardModal", (data) => {
+            insert_html(boards_list_id, data)
+        })
+    }
+    else closeModal('newBoardModal')
+
 }
--- website/static/js/modals/new_category.js
+++ website/static/js/modals/new_category.js
@@ -1,10 +1,11 @@
 function ok_newCategory() {
     let name = document.getElementById("categoryName").value;
 
-    make_modal_request({"new_cat_name": name}, new_cat_url, "newCatModal", (data) => {
-        console.log(data)
-        insert_html(categories_list_id, data)
-    })
-
-    closeModal("newCatModal")
+    if (name != '') {
+        make_modal_request({"new_cat_name": name}, new_cat_url, "newCatModal", (data) => {
+            console.log(data)
+            insert_html(categories_list_id, data)
+        })
+    }
+    else closeModal("newCatModal")
 }
