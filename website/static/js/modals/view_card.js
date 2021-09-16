b2a6184b34b0983ffe823cf836bbe850e5aec44d
Serena <serena.passini.19@gmail.com>
2021-09-16 21:28:52 +0200

    Ora si correttamente i valori in view card

--- website/static/js/modals/view_card.js
+++ website/static/js/modals/view_card.js
@@ -10,25 +10,8 @@ addOnShowListener("cardModal", e => {
 
     set_card_field("cardModalDescr", card_data.card_descr, "Nessuna descrizione")
     set_card_field("cardModalDate", card_data.card_date, "Nessuna data")
-    set_card_field('cardModalMember', card_data.card_members, 'Nessun membro assegnato')
-
-    let container = document.getElementById('cardModalCheck')
-    container.innerText= ""
-
-    for (const check in card_data.card_checks) {
-        let checkbox = document.createElement('input');
-        let label = document.createElement('label')
-        let checkText = document.createTextNode(check);
-
-        checkbox.type = 'checkbox';
-        checkbox.checked = card_data.card_checks[check]
-        
-        label.appendChild(checkbox);
-        label.appendChild(checkText);
-        label.classList.add("mb-2")
-
-        container.appendChild(label)
-    }
+    set_checklist_field()
+    set_member_field()
 })
 
 function set_card_field(el_id, value, no_value_text) {
@@ -43,6 +26,50 @@ function set_card_field(el_id, value, no_value_text) {
     }
 }
 
+function set_checklist_field() {
+    let container = document.getElementById('cardModalCheck')
+    container.innerText = ""
+
+    for (const check in card_data.card_checks) {
+        let checkbox = document.createElement('input');
+        let label = document.createElement('label')
+        let checkText = document.createTextNode(check);
+
+        checkbox.type = 'checkbox';
+        checkbox.checked = card_data.card_checks[check]
+
+        label.appendChild(checkbox);
+        label.appendChild(checkText);
+        label.classList.add("mb-2")
+
+        container.appendChild(label)
+        container.classList.remove("text-muted")
+    }
+
+    if (isEmpty(card_data.card_checks)) {
+        container.innerText = "Nessuna checkist"
+        container.classList.add("text-muted")
+    }
+
+}
+
+function set_member_field() {
+    let parent = document.getElementById('cardModalMember')
+    if (card_data.card_members) {
+        parent.innerText = card_data.card_members
+        parent.classList.remove("text-muted")
+    }
+
+    if (isEmpty(card_data.card_members)) {
+        parent.innerText = "Nessun membro assegnato"
+        parent.classList.add("text-muted")
+    }
+}
+
 function ok_viewCard() {
     closeModal("cardModal")
+}
+
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
 }
